0407fd23d83098e3d447add2aa95aa523cf05e2c
unknown <andersonyrosana13@gmail.com>
2023-11-05 22:27:33 -0400

    new Update in Project in date 02-11-2023

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
+  // @ViewChild('carousel', { static: true }) carousel: NgbCarousel;
 
- ngOnInit(): void {}
+ ngOnInit(): void {
+
+ }
   
 
 }
